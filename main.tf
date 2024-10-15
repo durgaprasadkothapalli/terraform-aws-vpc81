78bb53b76fc4ca7f653ab617ada6aafa49a0dd07
durgaprasadkothapalli <durgaprasadkothapalli908@gmail.com>
2024-10-15 14:04:28 +0530

    terra

--- main.tf
+++ main.tf
@@ -106,3 +106,93 @@ resource "aws_nat_gateway" "main" {
   # on the internet Gateway for the vpc.connection {
     depends_on = [ aws_internet_gateway.main ]
   }
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+
+  tags = merge(
+    var.common_tags,
+    var.public_route_table_tags,
+{
+    Name = "${local.resource_name}-public"
+}
+)
+}
+
+# resource "aws_route_table" "public" {
+#   vpc_id = aws_vpc.main.id
+
+
+#   tags = merge(
+#     var.common_tags,
+#     var.public_route_table_tags,
+# {
+#     Name = "${local_resource_name}-public"
+# }
+# )
+# }
+
+#private route-table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+
+  tags = merge(
+    var.common_tags,
+    var.private_route_table_tags,
+{
+    Name = "${local.resource_name}-private"
+}
+)
+}
+ #public route-table
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.database_route_table_tags,
+{
+    Name = "${local.resource_name}-database"
+}
+)
+}
+
+
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+gateway_id = aws_internet_gateway.main.id  
+}
+
+resource "aws_route" "private_nat" {
+  route_table_id = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+gateway_id = aws_nat_gateway.main.id  
+}
+
+resource "aws_route" "database_nat" {
+  route_table_id = aws_route_table.database.id
+  destination_cidr_block = "0.0.0.0/0"
+gateway_id = aws_nat_gateway.main.id  
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id = aws_subnet.public[count.index].id  
+  route_table_id =  aws_route_table.public.id
+  
+}
+
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidrs)
+  subnet_id = aws_subnet.private[count.index].id  
+  route_table_id =  aws_route_table.private.id
+  }
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnet_cidrs)
+  subnet_id = aws_subnet.database[count.index].id  
+  route_table_id =  aws_route_table.database.id
+  
+}
--- variables.tf
+++ variables.tf
@@ -78,3 +78,25 @@ variable "igw_tags" {
     default = {}
      
    }
+
+   variable "public_route_table_tags" {
+    default = {}
+     
+   }
+variable "private_route_table_tags" {
+default = {}
+  }
+
+  variable "database_route_table_tags" {
+default = {}
+  }
+
+  variable "is_peering_required" {
+       type = bool
+        default = false    
+  }
+
+  variable "vpc_peering_tags"{
+  default = {}
+    
+  }
